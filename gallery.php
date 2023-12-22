b61f03033a2cb078abf93104f7bf74b8c201bea2
PASIFIK <mr.pasifik@gmail.com>
2023-12-22 15:06:53 +0700

    update gallery

--- gallery.php
+++ gallery.php
@@ -34,32 +34,61 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 $sql = "SELECT * FROM photos WHERE user_id='$user_id'";
 $result = $conn->query($sql);
 ?>
-<link rel="stylesheet" type="text/css" href="css/gallery.css">
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gallery</title>
+    <link rel="stylesheet" type="text/css" href="css/gallery.css">
+    <!-- Include lightGallery styles -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/lightgallery@1.10.0/dist/css/lightgallery.min.css">
 </head>
+
 <body>
-    <h2>Gallery</h2>
+
     <a href="logout.php">Logout</a>
+
     <form method="post" action="" enctype="multipart/form-data">
-        <label>Description:</label>
-        <input type="text" name="description">
-        <br>
         <label>Upload Photo:</label>
         <input type="file" name="file" accept="image/*">
         <br>
+        <label>Description:</label>
+        <input type="text" name="description">
+        <br>
         <input type="submit" value="Upload">
     </form>
-    <br>
 
-    <?php
-    while ($row = $result->fetch_assoc()) {
-        echo '<img src="uploads/' . $row['filename'] . '" alt="' . $row['description'] . '" style="max-width: 200px; margin: 10px;">';
-    }
-    ?>
+    <h2>Gallery</h2>
+    <div id="lightgallery">
+        <?php
+        while ($row = $result->fetch_assoc()) {
+            echo '<a href="uploads/' . $row['filename'] . '" data-sub-html="' . $row['description'] . '">';
+            echo '<img src="uploads/' . $row['filename'] . '" alt="' . $row['description'] . '">';
+            echo '</a>';
+        }
+        ?>
+    </div>
+
+    <!-- Include jQuery from a CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <!-- Include lightGallery scripts from a CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/lightgallery@1.10.0/dist/js/lightgallery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/lightgallery@1.10.0/dist/js/plugins/lg-thumbnail.min.js"></script>
+
+    <script>
+        // Initialize lightGallery
+        $(document).ready(function () {
+            $("#lightgallery").lightGallery({
+                selector: 'a',
+                thumbnail: true,
+                download: false
+            });
+        });
+    </script>
 </body>
+
 </html>
